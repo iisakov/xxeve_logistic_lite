1b62e03899ff88148e2b46e6f8c889ac4bea74ac
iisakov <iisakov.artisan@yandex.ru>
2023-02-22 12:18:29 +0300

    Вывел на странице таблицу с данными как в консоли

--- server.py
+++ server.py
@@ -12,7 +12,7 @@ config = json.load(open('config.json'))
 @app.route('/', methods=['GET'])
 def index():
     path = 'index.html'
-    return render_template(path, as_attachment=True)
+    return render_template(path, title='Домашняя страница')
 
 
 @app.route('/sc/<string:mode>/<string:from_solar_system>/<string:to_solar_system>/', methods=['GET'])
@@ -20,7 +20,12 @@ def get_security_check(mode, from_solar_system, to_solar_system):
     mode = mode if mode in ['shortest', 'secure', 'insecure'] else config['CLI_param']['-sc']['sub_params']['scm']['default']
 
     system(f'./main.py -w -sc scm:{mode} scf:{from_solar_system} sct:{to_solar_system}')
-    return json.load(open(f'./test.json'))
+    path = 'security_check_table.html'
+    return render_template(path,
+                           title=f'{from_solar_system} - {to_solar_system}',
+                           from_solar_system=from_solar_system,
+                           to_solar_system=to_solar_system,
+                           data=json.load(open('test.json')))
 
 
 if __name__ == '__main__':
